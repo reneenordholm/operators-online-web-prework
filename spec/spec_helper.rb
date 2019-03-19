578969e6156118080140a5266b86d20eeb709f8c
Renee Nordholm <reneenordholm@gmail.com>
2019-03-19 23:46:21 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,8 @@
 require_relative '../lib/operations.rb'
 
 RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
   # config here
+  config.order = 'default'
 end
